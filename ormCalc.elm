57c12cd62885e3551b3017741ab4f045edd95b7f
Wojciech Drzazgowski <wojciech.drzazgowski@ihs.com>
2017-06-30 11:29:06 +0200

    got it to compile

--- ormCalc.elm
+++ ormCalc.elm
@@ -3,32 +3,48 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
 main =
-    Html.beginnerProgram { model = model, update = update, view = view }
+    Html.program { init = init, update = update, view = view, subscriptions = subscriptions }
+
+
 
 type alias Model =
     { repetitions: Int
     , weight: Int
     , oneRepMax: Int
     }
-model: Model
-    
+
+model: Model  
 model = 
     { repetitions = 0
     , weight = 0
     ,oneRepMax = 0
     }
 
+
+
+init: ( Model, Cmd Msg )
+init =
+    ({ repetitions = 1, weight = 1, oneRepMax = 1}, Cmd.none)
+
+
+
 type Msg =
     UpdateReps String
     | UpdateWeight String
 
-update: Msg -> Model -> Model
+update: Msg -> Model -> ( Model, Cmd Msg )
 update msg model = 
     case msg of
         UpdateReps reps ->
-            { model | repetitions = Result.withDefault 0 (String.toInt reps) }
+            ( { model | repetitions = Result.withDefault 0 (String.toInt reps) }, Cmd.none )
         UpdateWeight weight ->
-            { model | weight = Result.withDefault 0 (String.toInt weight) }
+            ( { model | weight = Result.withDefault 0 (String.toInt weight) }, Cmd.none )
+
+
+
+subscriptions: Model -> Sub Msg
+subscriptions model = 
+    Sub.none
 
 view: Model -> Html Msg
 view model =
